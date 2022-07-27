37454533aad6cd266971b13eccbe4bf70f769f2b
Katerina Skroumpelou <sk.katherine@gmail.com>
2022-07-27 16:20:01 +0300

    added modulefedarationplugin

--- apps/host/webpack.config.js
+++ apps/host/webpack.config.js
@@ -1,6 +1,13 @@
 const withModuleFederation = require('@nrwl/react/module-federation');
 const moduleFederationConfig = require('./module-federation.config');
+const { ModuleFederationPlugin } = require('webpack').container;
 
 module.exports = withModuleFederation({
   ...moduleFederationConfig,
+  plugins: [
+    new ModuleFederationPlugin({
+      // options' typings in typescript
+      runtime: 'string' | false,
+    }),
+  ],
 });
